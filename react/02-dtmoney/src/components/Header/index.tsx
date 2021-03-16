eb947e882edf5527074fdaaebfe87e566f4bf1ca
glauberc <glaubercarv@gmail.com>
2021-03-16 14:01:28 -0300

    add

---
+++ react/02-dtmoney/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import logoimg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+export function Header() {
+  return (
+    <Container>
+      <Content>
+        <img src={logoimg} alt="" />
+        <button type="button">Nova transação</button>
+      </Content>
+    </Container>
+  );
+}
